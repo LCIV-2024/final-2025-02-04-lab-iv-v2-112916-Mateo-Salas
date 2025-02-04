56aa4fd5642d85c8463e02f038ec445dc4292576
112916-Mateo-Salas <111440978+112916-Mateo-Salas@users.noreply.github.com>
2025-02-04 17:59:07 -0300

    update test

---
+++ src/test/java/ar/edu/utn/frc/tup/lciii/controllers/DeviceControllerTest.java
@@ -0,0 +1,17 @@
+package ar.edu.utn.frc.tup.lciii.controllers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+@SpringBootTest
+class DeviceControllerTest {
+
+    @Test
+    void postDevice() {
+    }
+
+    @Test
+    void getDevicesByType() {
+    }
+}
